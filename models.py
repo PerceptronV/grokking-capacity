8fbba706bef3a7efead170aa80a4a97bc0b7cc27
Vincent <neutrinovs@gmail.com>
2026-03-28 12:35:39 -0400

    Vendor RoPE

--- models.py
+++ models.py
@@ -3,7 +3,9 @@
 #       https://github.com/atveit/torch_grokking (MIT License)
 # which itself is a PyTorch port of the original MLX code by Jason Stock, available at
 #       https://github.com/stockeh/mlx-grokking
-# We have not modified this file.
+# RotaryPositionalEmbeddings vendored verbatim from torchtune v0.1.1
+#       https://github.com/pytorch/torchtune (BSD-style license)
+#       to remove the torchtune runtime dependency while preserving identical behaviour.
 # ---------------------------------------------
 
 
@@ -13,10 +15,53 @@
 #from mlx.utils import tree_flatten, tree_map
 
 # --- New: Torch imports ---
+from typing import Optional
 import torch
 import torch.nn as nn_torch
 import torch.nn.functional as F
-from torchtune.modules.position_embeddings import RotaryPositionalEmbeddings
+from torch import Tensor
+
+
+# Vendored verbatim from torchtune v0.1.1 (torchtune/modules/position_embeddings.py)
+# Copyright (c) Meta Platforms, Inc. and affiliates. BSD-style license.
+class RotaryPositionalEmbeddings(nn_torch.Module):
+    def __init__(self, dim: int, max_seq_len: int = 4096, base: int = 10_000) -> None:
+        super().__init__()
+        self.dim = dim
+        self.base = base
+        self.max_seq_len = max_seq_len
+        self._rope_init()
+
+    def reset_parameters(self):
+        self._rope_init()
+
+    def _rope_init(self):
+        theta = 1.0 / (
+            self.base
+            ** (torch.arange(0, self.dim, 2)[: (self.dim // 2)].float() / self.dim)
+        )
+        self.register_buffer("theta", theta, persistent=False)
+        self.build_rope_cache(self.max_seq_len)
+
+    def build_rope_cache(self, max_seq_len: int = 4096) -> None:
+        seq_idx = torch.arange(max_seq_len, dtype=self.theta.dtype, device=self.theta.device)
+        idx_theta = torch.einsum("i, j -> ij", seq_idx, self.theta).float()
+        cache = torch.stack([torch.cos(idx_theta), torch.sin(idx_theta)], dim=-1)
+        self.register_buffer("cache", cache, persistent=False)
+
+    def forward(self, x: Tensor, input_pos: Optional[Tensor] = None) -> Tensor:
+        seq_len = x.size(1)
+        rope_cache = self.cache[:seq_len] if input_pos is None else self.cache[input_pos]
+        xshaped = x.float().reshape(*x.shape[:-1], -1, 2)
+        rope_cache = rope_cache.view(1, xshaped.size(1), 1, xshaped.size(3), 2)
+        x_out = torch.stack(
+            [
+                xshaped[..., 0] * rope_cache[..., 0] - xshaped[..., 1] * rope_cache[..., 1],
+                xshaped[..., 1] * rope_cache[..., 0] + xshaped[..., 0] * rope_cache[..., 1],
+            ],
+            -1,
+        )
+        return x_out.flatten(3).type_as(x)
 
 class AttentionTorch(nn_torch.Module):
     def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
